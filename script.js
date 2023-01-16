035c615c320b3cc66fbb7a5cda02a88d8f9560cd
anhtuv <anhtuvothang@gmail.com>
2023-01-16 02:54:58 -0500

    Home, About me, Project updates

--- script.js
+++ script.js
@@ -8,3 +8,44 @@ document.addEventListener('DOMContentLoaded', function() {
     
     })  
 }, false);
+
+
+function TxtType(element) {
+    this.element = element;
+    this.txt = '';
+    this.tick();
+    // this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var fullTxt = "Welcome! Bienvenue!  ";
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } 
+    else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.element.innerHTML = '<span>'+this.txt+'</span>';
+
+    var that = this;
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        this.isDeleting = true;
+        //this.element.style.borderRight = 0;
+    } 
+    else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+
+    }
+
+    setTimeout(function() {that.tick();}, 200);
+
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    new TxtType(elements[0]);
+    console.log(elements[0]);
+};
